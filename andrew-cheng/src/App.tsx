de481fb65d7e23e9606ad13c9ac8bef5ec4768a2
Andrew-Cheng <175060524+Andrew-Cheng@users.noreply.github.com>
2025-02-11 02:05:30 -0500

    fix typescript errors

--- andrew-cheng/src/App.tsx
+++ andrew-cheng/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
-import About from './pages/About';
 import Projects from './pages/Projects';
 
 function App() {
