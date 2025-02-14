72a72c5b9c3037e5b5b337e3c123a5d732bbd86f
Andrew-Cheng <175060524+Andrew-Cheng@users.noreply.github.com>
2025-02-14 13:08:18 -0500

    add analytics

--- andrew-cheng/src/App.tsx
+++ andrew-cheng/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <Router>
@@ -14,6 +14,7 @@ function App() {
             {/* <Route path="/about" element={<About />} /> */}
             <Route path="/projects" element={<Projects />} />
           </Routes>
+          <Analytics />
         </main>
       </div>
     </Router>
